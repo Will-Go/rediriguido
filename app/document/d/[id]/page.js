c6ee41cb3e4cdf93335015d4bb7835dd967e278a
Wilson Gong Wu <wgongw138@ulacit.ed.cr>
2023-11-09 11:18:51 -0600

    RemovedFeature: Modified Title

--- app/document/d/[id]/page.js
+++ app/document/d/[id]/page.js
@@ -5,6 +5,10 @@ import { useEffect, useState } from 'react';
 import {db} from '@/config/firebaConfig'
 import {collection, addDoc, Timestamp } from 'firebase/firestore'
 import { useSearchParams } from 'next/navigation';
+import Head from 'next/head'
+
+
+
 
 function Test({params}) {
 
@@ -15,13 +19,32 @@ function Test({params}) {
 
   const [ip, setIp] = useState();
 
+  // const [title, setTitle] = useState("");
+
   const page = searchParams.get('usp');
 
     useEffect(()=>{
-      
-
+      // async function getPageTitle() {
+      //   try {
+      //     const response = await fetch(`https://docs.google.com/document/d/${params.id}/edit?usp=sharing`);
+      //     if (response.ok) {
+      //       const html = await response.text();
+      //       const titleMatch = /<title>(.*?)<\/title>/i.exec(html);
+      //       const pageTitle = titleMatch ? titleMatch[1] : 'Title not found';
+      //       console.log(pageTitle);
+      //       setTitle(pageTitle);
+      //       document.title = pageTitle;
+      //     } else {
+      //       throw new Error('Network response was not ok.');
+      //     }
+      //   } catch (error) {
+      //     throw new Error('Error fetching the page:', error);
+      //   }
+      // }
+      // getPageTitle();
 
       async function ipFetch(){
+
         await fetch('https://api.ipify.org?format=json')
         .then(response => response.json())
         .then(async data => {
@@ -82,7 +105,8 @@ function Test({params}) {
     }, [])
 
   return (
-    <div className='flex flex-col m-3 justify-center items-center h-screen'>
+    <div className='flex flex-col m-3 justify-center items-center h-screen'>    
+       
         {/* {params.id}
         <div>
         {ip}
